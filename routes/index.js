5cbe16a5672411b2390bb2f644ab7d3ca8977bef
root <root@coltenj.com>
2017-07-01 15:48:12 +0000

    site backup

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 
 router.use(express.static('sites/coltenj.com/'))
 
+router.get('/holo', (req,res) => res.redirect('https://medium.com/@colten_jackson/hello-hololens-creating-holographic-animations-with-python-blender-44d8ba237a2b'))
+
 //if the route reaches this catchall, send 404
 router.get('*', (req,res,next)=>{
   res.sendStatus(404);
